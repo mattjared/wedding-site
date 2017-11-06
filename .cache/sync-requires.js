6a3241e998fd1ce92fa285acac4e2c0449dcadc8
Matt Jared <mjared@thezebra.com>
2017-11-06 09:45:02 -0600

    Setup gatsby

---
+++ .cache/sync-requires.js
@@ -0,0 +1,24 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-pages-index-js": preferDefault(require("/Users/mattjared/Projects/wedding-site/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/mattjared/Projects/wedding-site/src/pages/page-2.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/mattjared/Projects/wedding-site/src/pages/404.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/layout-index.json"),
+  "index.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/index.json"),
+  "layout-index.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/layout-index.json"),
+  "page-2.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/page-2.json"),
+  "layout-index.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/layout-index.json"),
+  "404.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/404.json"),
+  "layout-index.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/layout-index.json"),
+  "404-html.json": require("/Users/mattjared/Projects/wedding-site/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": preferDefault(require("/Users/mattjared/Projects/wedding-site/.cache/layouts/index.js"))
+}
