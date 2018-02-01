2e15a8adcfeede40b42d6b04ea059f8475cbd367
Matt Jared <mattjared9@gmail.com>
2018-01-31 20:19:14 -0600

    Logic to hide portion of form for No rsvps

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,24 @@
-"use strict";
+'use strict';
 
-var app = function app() {
-  return console.log("This app was started at " + Date.now().toLocaleString() + "!");
-};
+var rsvpField = document.getElementsByName('attending_wedding');
+var attendingFields = document.querySelector('.attending-form');
+var notAttendingFields = document.querySelector('.not-attending-form');
 
-var myApp = app();
+function showCorrectFields() {
+  console.log(event.target.value);
+  if (event.target.value === 'Yes') {
+    attendingFields.classList.add('show');
+    if (notAttendingFields != null) {
+      notAttendingFields.classList.remove('show');
+    }
+  } else if (event.target.value === 'No') {
+    notAttendingFields.classList.add('show');
+    if (attendingFields != null) {
+      attendingFields.classList.remove('show');
+    }
+  }
+}
+
+for (var i = 0; i < rsvpField.length; i += 1) {
+  rsvpField[i].addEventListener("change", showCorrectFields);
+}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,22 @@
-var app = () => console.log(`This app was started at ${Date.now().toLocaleString()}!`);
+const rsvpField = document.getElementsByName('attending_wedding');
+const attendingFields = document.querySelector('.attending-form');
+const notAttendingFields = document.querySelector('.not-attending-form');
 
-var myApp = app();
+function showCorrectFields() {
+  console.log(event.target.value);
+  if (event.target.value === 'Yes') {
+    attendingFields.classList.add('show');
+    if (notAttendingFields != null) {
+      notAttendingFields.classList.remove('show');
+    }
+  } else if (event.target.value === 'No') {
+    notAttendingFields.classList.add('show');
+    if (attendingFields != null) {
+      attendingFields.classList.remove('show');
+    }
+  }
+}
+
+for (let i = 0; i < rsvpField.length; i += 1) {
+  rsvpField[i].addEventListener("change", showCorrectFields);
+}
