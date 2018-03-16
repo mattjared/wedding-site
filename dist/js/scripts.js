2746e06b1c80fc7aa13598e5f2b32600a464e9e6
Matt Jared <mattjared9@gmail.com>
2018-03-15 23:02:17 -0500

    content updates

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,24 +1,23 @@
-'use strict';
+// const rsvpField = document.getElementsByName('attending_wedding');
+// const attendingFields = document.querySelector('.attending-form');
+// const notAttendingFields = document.querySelector('.not-attending-form');
 
-var rsvpField = document.getElementsByName('attending_wedding');
-var attendingFields = document.querySelector('.attending-form');
-var notAttendingFields = document.querySelector('.not-attending-form');
+// console.log('Stop reading my code. Email me w/ bugs, ya jabroni!')
+// function showCorrectFields() {
+//   if (event.target.value === 'Yes') {
+//     attendingFields.classList.add('show');
+//     if (notAttendingFields != null) {
+//       notAttendingFields.classList.remove('show');
+//     }
+//   } else if (event.target.value === 'No') {
+//     notAttendingFields.classList.add('show');
+//     if (attendingFields != null) {
+//       attendingFields.classList.remove('show');
+//     }
+//   }
+// }
 
-console.log('Stop reading my code. Email me w/ bugs, ya jabroni!');
-function showCorrectFields() {
-  if (event.target.value === 'Yes') {
-    attendingFields.classList.add('show');
-    if (notAttendingFields != null) {
-      notAttendingFields.classList.remove('show');
-    }
-  } else if (event.target.value === 'No') {
-    notAttendingFields.classList.add('show');
-    if (attendingFields != null) {
-      attendingFields.classList.remove('show');
-    }
-  }
-}
-
-for (var i = 0; i < rsvpField.length; i += 1) {
-  rsvpField[i].addEventListener("change", showCorrectFields);
-}
+// for (let i = 0; i < rsvpField.length; i += 1) {
+//   rsvpField[i].addEventListener("change", showCorrectFields);
+// }
+"use strict";
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,22 +1,22 @@
-const rsvpField = document.getElementsByName('attending_wedding');
-const attendingFields = document.querySelector('.attending-form');
-const notAttendingFields = document.querySelector('.not-attending-form');
+// const rsvpField = document.getElementsByName('attending_wedding');
+// const attendingFields = document.querySelector('.attending-form');
+// const notAttendingFields = document.querySelector('.not-attending-form');
 
-console.log('Stop reading my code. Email me w/ bugs, ya jabroni!')
-function showCorrectFields() {
-  if (event.target.value === 'Yes') {
-    attendingFields.classList.add('show');
-    if (notAttendingFields != null) {
-      notAttendingFields.classList.remove('show');
-    }
-  } else if (event.target.value === 'No') {
-    notAttendingFields.classList.add('show');
-    if (attendingFields != null) {
-      attendingFields.classList.remove('show');
-    }
-  }
-}
+// console.log('Stop reading my code. Email me w/ bugs, ya jabroni!')
+// function showCorrectFields() {
+//   if (event.target.value === 'Yes') {
+//     attendingFields.classList.add('show');
+//     if (notAttendingFields != null) {
+//       notAttendingFields.classList.remove('show');
+//     }
+//   } else if (event.target.value === 'No') {
+//     notAttendingFields.classList.add('show');
+//     if (attendingFields != null) {
+//       attendingFields.classList.remove('show');
+//     }
+//   }
+// }
 
-for (let i = 0; i < rsvpField.length; i += 1) {
-  rsvpField[i].addEventListener("change", showCorrectFields);
-}
+// for (let i = 0; i < rsvpField.length; i += 1) {
+//   rsvpField[i].addEventListener("change", showCorrectFields);
+// }
